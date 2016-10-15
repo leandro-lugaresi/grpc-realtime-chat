34376036ddf5db7cbbfd186fd9a877559aae8a30
Leandro Lugaresi <leandrolugaresi92@gmail.com>
2016-10-15 17:17:34 -0300

    change userManager to mysql

--- server/user/mysql/userManager.go
+++ server/user/mysql/userManager.go
@@ -1,17 +1,39 @@
 package mysql
 
 import (
-	"github.com/boltdb/bolt"
+	"database/sql"
 
 	"github.com/leandro-lugaresi/grpc-realtime-chat/server/user"
 )
 
 type UserManager struct {
-	db *bolt.DB
+	db *sql.DB
 }
 
 func (m UserManager) GetUserByUsername(username string) (*user.User, error) {
+	user := &user.User{}
 
+	rows, err := db.Query("SELECT id, name, username, password,  FROM users WHERE username=?", username)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		if err := rows.Scan(&rawUser); err != nil {
+			return nil, err
+		}
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	err = proto.Unmarshal(rawUser, user)
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
 }
 func (m UserManager) GetUserById(id string) (*user.User, error) {
 
